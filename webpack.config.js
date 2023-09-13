fffb92272397d1d5e656de72ef7c409373d407c2
Zainal Abidin <zainalabidinhsc@gmail.com>
2023-09-13 07:22:09 +0700

    add htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -7,4 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
