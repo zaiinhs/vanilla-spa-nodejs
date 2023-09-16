52c0ce51f34cbf22efd7a344273c9bdb711c378f
Zainal Abidin <zainalabidinhsc@gmail.com>
2023-09-16 18:35:50 +0700

    add concurrently, webpack merge, and separated file

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  watch: true,
-  mode: "development",
   entry: "./src/client/index.js",
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "build"),
-  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
